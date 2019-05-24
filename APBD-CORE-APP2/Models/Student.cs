f1826e19efbd2e3833f7db326b53637d8c0eb5ca
Imerlith <36335136+Imerlith@users.noreply.github.com>
2019-05-24 19:51:07 +0200

    Zadanie 2

--- APBD-CORE-APP2/Models/Student.cs
+++ APBD-CORE-APP2/Models/Student.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace APBD_CORE_APP2.Models
 {
     public class Student
     {
+        [Display(Name ="Imię")]
         public string FirstName { get; set; }
+        [Display(Name = "Nazwisko")]
         public string LastName { get; set; }
+        [Display(Name = "Kierunek")]
         public string StudyName { get; set; }
     }
 }
